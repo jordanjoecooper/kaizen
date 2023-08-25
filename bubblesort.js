b8f70e487831b1eeb1975eadf7ce79541a2dea70
Jordan Joe Cooper <32134629+jordanjoecooper@users.noreply.github.com>
2023-08-25 15:55:59 +0100

    Update bubblesort.js

--- bubblesort.js
+++ bubblesort.js
@@ -1,13 +1,5 @@
+// Bubblesort
 const data = [10,99,59,9,2,8,11,7,14,16,6, 12,111111,567,321554,78,13,1,88,667,09,532,19,5,7,3,78,44,33,22,77,4353,213123,676767]
 
 const sortData = data.sort((a, b) => a - b)
 console.log(sortData)
-
-/*
-Given an array of numbers in a random order.
-Sort them using bubblesort into their ascending order 0-10
-
-What does sort do?
-
-Sorts an array in place. This method mutates the array and returns a reference to the same array.
-*/
